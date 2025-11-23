5c197827858f79e061ee33733e6f64746c382a08
NemoRed <svetik260705@gmail.com>
2025-11-23 19:57:47 +0300

    Add GameEngine and basic rules

---
+++ app/src/main/java/com/mirea/nardymobile/logic/GameEngine.kt
@@ -0,0 +1,34 @@
+package com.mirea.nardymobile.logic
+
+import com.mirea.nardymobile.models.Chip
+import com.mirea.nardymobile.models.Player
+
+object GameEngine {
+
+    var currentPlayer: Player? = null
+        private set
+    var dice: Pair<Int, Int>? = null
+        private set
+
+    fun startGame(player1: Player, player2: Player) {
+        currentPlayer = player1
+        dice = null
+        Board.reset()
+    }
+
+    fun rollDice() {
+        dice = Dice.roll()
+    }
+
+    fun makeMove(chip: Chip, steps: Int): Boolean {
+        if (chip.color != currentPlayer?.color) return false
+
+        chip.position += steps
+        return true
+    }
+
+
+    fun switchPlayer(p1: Player, p2: Player) {
+        currentPlayer = if (currentPlayer == p1) p2 else p1
+    }
+}
